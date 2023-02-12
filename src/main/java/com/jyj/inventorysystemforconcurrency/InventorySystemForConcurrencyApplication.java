47e18dd9471178436b61aed56ea7e4674115f9c8
yoonjeong <cyj199637@gmail.com>
2023-02-12 18:17:23 +0900

    (chore) : init setting

---
+++ src/main/java/com/jyj/inventorysystemforconcurrency/InventorySystemForConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.jyj.inventorysystemforconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemForConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySystemForConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyj/inventorysystemforconcurrency/InventorySystemForConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyj.inventorysystemforconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemForConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
