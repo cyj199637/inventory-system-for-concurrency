07d0b5721b8ec003bed8ff918b9c78a949870543
yoonjeong <cyj199637@gmail.com>
2023-02-13 06:35:02 +0900

    (test) : 동시에 100개의 요청이 들어오는 경우 테스트 케이스 추가

--- src/test/java/com/jyj/inventorysystemforconcurrency/stock/service/StockServiceTest.java
+++ src/test/java/com/jyj/inventorysystemforconcurrency/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +40,7 @@ class StockServiceTest {
     void decrease() {
         stockService.decrease(1L, 2L);
 
-        assertThat(stockService.findById(1L).getQuantity()).isEqualTo(48L);
+        assertThat(stockService.findById(1L).getQuantity()).isEqualTo(98L);
     }
 
     @Test
@@ -51,8 +54,33 @@ class StockServiceTest {
     @Test
     @DisplayName("현재 재고가 입력 값보다 적으면 재고 감소하는데 실패한다.")
     void fail_to_decrease_with_current_stock() {
-        assertThatThrownBy(() -> stockService.decrease(1L, 100L))
+        assertThatThrownBy(() -> stockService.decrease(1L, 200L))
             .isInstanceOf(RuntimeException.class)
             .hasMessage("재고가 부족합니다.");
+    }
+
+    @Test
+    void 동시에_100개_요청이_들어오는_경우() throws InterruptedException {
+        int totalThreadCount = 100;
+
+        // ExecutorService: 비동기로 실행하는 작업을 단순화하여 사용할 수 있게 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // CountDownLatch: 다른 스레드에서 수행 중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(totalThreadCount);
+
+        for (int i = 0; i < totalThreadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        // 레이스 컨디션이 발생하여 테스트 실패
+        assertThat(stockService.findById(1L).getQuantity()).isEqualTo(0);
     }
 }
--- src/test/resources/sql/init.sql
+++ src/test/resources/sql/init.sql
@@ -1,5 +1,5 @@
 INSERT INTO stock (id, product_id, quantity)
-VALUES (1, 1, 50);
+VALUES (1, 1, 100);
 
 INSERT INTO stock (id, product_id, quantity)
 VALUES (2, 2, 0);
